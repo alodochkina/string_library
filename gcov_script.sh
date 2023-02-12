f84da17eff584c5b2b30cac47c8fc4a8dccf7c06
koshkara <lodochkina.a@ya.ru>
2023-02-12 19:15:40 +0100

    test gcov_report actions

--- gcov_script.sh
+++ gcov_script.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 report=$(make gcov_report)
-echo "$report"
-coverage_lines=$(awk '/lines:/' "$report" | awk '{print $2}')
+coverage_lines=$(awk '/lines/' "$report" | awk '{print $2}')
 echo "$coverage_lines"
-coverage_functions=$(awk '/functions:/' "$report" | awk '{print $2}')
+echo"THISISHER"
+coverage_functions=$(awk '/functions/' "$report" | awk '{print $2}')
+echo "HER"
 echo "$coverage_functions"
 if [ "$coverage_lines" -gt 80 ] && [ "$coverage_functions" -gt 80 ]
 then return 0
