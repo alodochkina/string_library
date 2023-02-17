8c934dd084eecafeeceafa4dd5f26c66622a8c87
koshkara <lodochkina.a@ya.ru>
2023-02-17 16:05:12 +0100

    test header actions

--- s21_string.h
+++ s21_string.h
@@ -5,7 +5,7 @@
 //#include <math.h>
 //#include <stdarg.h>
 #include <stdio.h>
-//#include <stdlib.h>
+#include <stdlib.h>
 // #include <string.h>
 //#include <limits.h> // Для max и min значений типов
 
