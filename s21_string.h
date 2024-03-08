4d70a52e6261391a089bcf1a4b8a5b8ffdf05c4e
koshkara <lodochkina.a@proton.me>
2024-03-08 10:37:09 +0100

    clang_nova

--- s21_string.h
+++ s21_string.h
@@ -2,11 +2,42 @@
 #define SRC_S21_STRING_H_
 
 #include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
 
 #define s21_NULL ((void *)0)
 typedef unsigned long s21_size_t;
 
+enum flags { no_flag,  minus, plus, space, number_sign, null };
+enum widths { no_width, number, asteriks };
+enum precisions { no_precision, point_number, point_asteriks };
+enum lengths { no_length, h, l, L };
+enum specifiers { no_specifier, c, d, i, e, E, f, g, G, o, s, u, x, X, p, n, percent};
+
+struct s21_parse_format {
+  enum flags flag;
+  enum widths width;
+  int width_num;
+  enum precisions precision;
+  int precision_num;
+  enum lengths length;
+  enum specifiers specifier;
+};
+
+typedef struct s21_parse_format S21_parse_format;
+
+struct s21_forma {
+  char* str_format;
+  char* str_argument;
+  S21_parse_format parser;
+  struct s21_forma* next_format;
+};
+
+typedef struct s21_forma S21_forma;
+
 enum result { not, equal, good };
+
+
 void *s21_memchr(const void *str, int c, s21_size_t n);
 int s21_memcmp(const void *str1, const void *str2, s21_size_t n);
 void *s21_memcpy(void *dest, const void *src, s21_size_t n);
@@ -28,4 +59,12 @@ s21_size_t s21_strspn(const char *str1, const char *str2);
 char *s21_strstr(const char *haystack, const char *needle);
 char *s21_strtok(char *str, const char *delim);
 
+int s21_sprintf(char *str, const char* format, ...);
+void check_format(const char* format, S21_forma* curr_point);
+void parser_format(S21_forma* curr_point);
+void check_flags(S21_forma* curr_point, int* i);
+void check_width(S21_forma* curr_point, int* i);
+void check_precision(S21_forma* curr_point, int* i);
+void check_length(S21_forma* curr_point, int* i);
+void check_specifier(S21_forma* specifier, int* i);
 #endif //  SRC_S21_STRING_H_
